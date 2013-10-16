d24a7c8bb00847b5fe71223ed36622a505643a3c
Josh Rolstad <jrolstad@gmail.com>
2013-10-16 06:33:14 -0700

    Implementing get method of controller

--- vigilo.app.services.web/Controllers/MessageQueueStatusController.cs
+++ vigilo.app.services.web/Controllers/MessageQueueStatusController.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using vigilo.app.services.web.Models.api;
+using vigilo.domain.services.Commands;
+using vigilo.domain.services.Interfaces;
 
 namespace vigilo.app.services.web.Controllers
 {
     public class MessageQueueStatusController : ApiController
     {
+        private readonly ICommand<GetRabbitMqServerUrlRequest, GetRabbitMqServerUrlResponse> _rabbitServerUrlCommand;
+        private readonly ICommand<GetMonitorableQueuesRequest, GetMonitorableQueuesReponse> _getQueuesToMonitorCommand;
+        private readonly ICommand<GetRabbitMqQueueMetadataRequest, GetRabbitMqQueueMetadataResponse> _getQueueMetaDataCommand;
+        private readonly IMapper<RabbitMqQueueMetadata, MessageQueueStatus> _messageQueueStatusMapper;
+
+        public MessageQueueStatusController(
+            ICommand<GetRabbitMqServerUrlRequest,GetRabbitMqServerUrlResponse> rabbitServerUrlCommand,
+            ICommand<GetMonitorableQueuesRequest,GetMonitorableQueuesReponse> getQueuesToMonitorCommand,
+            ICommand<GetRabbitMqQueueMetadataRequest,GetRabbitMqQueueMetadataResponse> getQueueMetaDataCommand,
+            IMapper<RabbitMqQueueMetadata,MessageQueueStatus> messageQueueStatusMapper)
+        {
+            _rabbitServerUrlCommand = rabbitServerUrlCommand;
+            _getQueuesToMonitorCommand = getQueuesToMonitorCommand;
+            _getQueueMetaDataCommand = getQueueMetaDataCommand;
+            _messageQueueStatusMapper = messageQueueStatusMapper;
+        }
+
         // GET api/messagequeuestatus
         public IEnumerable<MessageQueueStatus> Get()
         {
-            throw new NotImplementedException();
+            var urlResponse = _rabbitServerUrlCommand.Execute(new GetRabbitMqServerUrlRequest());
+            var queuesToMonitor = _getQueuesToMonitorCommand.Execute(new GetMonitorableQueuesRequest());
+
+            var metaDataRequest = new GetRabbitMqQueueMetadataRequest
+            {
+                RabbitMqUrl = urlResponse.Url,
+                QueueNames = queuesToMonitor.QueueNames
+            };
+            var metadata = _getQueueMetaDataCommand.Execute(metaDataRequest);
+
+            var models = metadata.Queues
+                .Select(_messageQueueStatusMapper.Map)
+                .ToList();
+
+            return models;
         }
 
         // GET api/messagequeuestatus/5
